648205af8b9007fd5dde7b2eaaf39f67b232393b
Hansanghyeon <999@hyeon.pro>
2024-06-11 13:08:02 +0000

    feat: plop hbs추가

---
+++ plopfile.js
@@ -0,0 +1,100 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default function (plop) {
+  plop.setGenerator('react-query (get)', {
+    description: 'Create a new API function',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your API function name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: `${__dirname}/src/api/{{kebabCase name}}/index.ts`,
+        templateFile: `${__dirname}/react-query-template/get/index.hbs`,
+      },
+      ...['api', 'dto', 'query', 'type'].map(file => ({
+        type: 'add',
+        path: `${__dirname}/src/api/{{kebabCase name}}/{{kebabCase name}}.${file}.ts`,
+        templateFile: `${__dirname}/react-query-template/get/${file}.hbs`,
+      }))
+    ],
+  });
+
+  plop.setGenerator('react-query (post)', {
+    description: 'Create a new API function',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your API function name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: `${__dirname}/src/api/{{kebabCase name}}/index.ts`,
+        templateFile: `${__dirname}/react-query-template/post/index.hbs`,
+      },
+      ...['api', 'dto', 'rdo', 'query', 'type'].map(file => ({
+        type: 'add',
+        path: `${__dirname}/src/api/{{kebabCase name}}/{{kebabCase name}}.${file}.ts`,
+        templateFile: `${__dirname}/react-query-template/post/${file}.hbs`,
+      }))
+    ],
+  });
+
+  plop.setGenerator('react-query (patch)', {
+    description: 'Create a new API function',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your API function name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: `${__dirname}/src/api/{{kebabCase name}}/index.ts`,
+        templateFile: `${__dirname}/react-query-template/patch/index.hbs`,
+      },
+      ...['api', 'dto', 'rdo', 'query', 'type'].map(file => ({
+        type: 'add',
+        path: `${__dirname}/src/api/{{kebabCase name}}/{{kebabCase name}}.${file}.ts`,
+        templateFile: `${__dirname}/react-query-template/patch/${file}.hbs`,
+      }))
+    ],
+  });
+  
+
+  plop.setGenerator('react-query (CRUD)', {
+    description: 'Create a new API function',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your API function name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: `${__dirname}/src/api/{{kebabCase name}}/index.ts`,
+        templateFile: `${__dirname}/react-query-template/crud/index.hbs`,
+      },
+      ...['api', 'dto', 'rdo', 'query', 'type'].map(file => ({
+        type: 'add',
+        path: `${__dirname}/src/api/{{kebabCase name}}/{{kebabCase name}}.${file}.ts`,
+        templateFile: `${__dirname}/react-query-template/crud/${file}.hbs`,
+      }))
+    ],
+  });
+}
